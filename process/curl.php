2df69fa5dd6bf4cef5140467f3db67cf8888c5b6
Mrxxm <362190221@qq.com>
2020-07-16 13:52:10 +0800

    提交

--- process/curl.php
+++ process/curl.php
@@ -15,13 +15,38 @@ $urls = [
 //foreach ($urls as $url) {
 //    $content[] = file_get_contents($url);
 //}
+$workers = [];
 
 for ($i = 0; $i < 6; $i++) {
     // 子进程
-    $process = new Process(function (Process $process) {
-
+    $process = new Process(function (Process $process) use($i, $urls) {
+        // 请求url
+        $content = curlData($urls[$i]);
+        // 输出到管道
+        echo $content . PHP_EOL;
     }, true);
 
     $pid = $process->start();
     $workers[$pid] = $process;
+}
+
+/*
+ * 输出管道内容
+ */
+foreach ($workers as $pid => $process) {
+    // 输出管道内容
+    echo $process->read();
+}
+
+/**
+ * 模拟请求URL的内容
+ * @param $url
+ * @return string
+ */
+function curlData($url)
+{
+    // curl file_get_contents
+    sleep(2);
+
+    return $url . "success" . PHP_EOL;
 }
