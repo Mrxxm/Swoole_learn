25243aaea8688b0c46c669f03d436edaafe21a53
Mrxxm <362190221@qq.com>
2020-07-16 13:45:37 +0800

    提交

--- process/curl.php
+++ process/curl.php
@@ -1,1 +1,27 @@
 <?php
+
+use Swoole\Process;
+
+$urls = [
+    'http://baidu.com',
+    'http://qq.com',
+    'http://sina.com.cn',
+    'http://baidu.com',
+    'http://baidu.com',
+    'http://baidu.com',
+];
+
+// 原始使用
+//foreach ($urls as $url) {
+//    $content[] = file_get_contents($url);
+//}
+
+for ($i = 0; $i < 6; $i++) {
+    // 子进程
+    $process = new Process(function (Process $process) {
+
+    }, true);
+
+    $pid = $process->start();
+    $workers[$pid] = $process;
+}
