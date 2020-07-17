b39d4ad42f9244d440cf4490118ce73458cae7f9
Mrxxm <362190221@qq.com>
2020-07-17 23:09:20 +0800

    提交

--- coroutine/redis.php
+++ coroutine/redis.php
@@ -1,1 +1,20 @@
 <?php
+
+//高性能HTTP服务器
+$http = new Swoole\Http\Server("127.0.0.1", 9501);
+
+$http->on("request", function ($request, $response) {
+
+    $redis = new Co\Redis;
+    $redis->connect('127.0.0.1', 6379); // 若是本地UNIXSocket则host参数应以形如`unix://tmp/xxx.sock`的格式填写
+
+    // 接收-设值
+    $redis->set('Coroutine:xxm', $request->get['a']);
+    $value = $redis->get('Coroutine:xxm');
+
+    // 返回-传值
+    $response->header('Content-Type', 'text/plain');
+    $response->end($value);
+});
+
+$http->start();
