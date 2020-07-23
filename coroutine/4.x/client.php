716f0bc6e44001d7e644ce009574ad99d0986a0b
Mrxxm <362190221@qq.com>
2020-07-23 15:10:42 +0800

    提交

--- coroutine/4.x/client.php
+++ coroutine/4.x/client.php
@@ -1,11 +1,12 @@
 <?php
 
-$client = new Swoole\Client(SWOOLE_SOCK_TCP);
-
-if (!$client->connect('127.0.0.1', 9501, -1)) {
-    exit("connect failed. Error: {$client->errCode}\n");
-}
-
-$client->send("协程客户端测试！\n");
-echo $client->recv();
-$client->close();
+Co\run(function(){
+    $client = new Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
+    if (!$client->connect('127.0.0.1', 9501, 0.5))
+    {
+        echo "connect failed. Error: {$client->errCode}\n";
+    }
+    $client->send("协程客户端测试！\n");
+    echo $client->recv();
+    $client->close();
+});
