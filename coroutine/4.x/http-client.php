e9209ef31ebdf31349995045b9ff191b14f892fb
Mrxxm <362190221@qq.com>
2020-07-23 16:53:28 +0800

    swoole 协程 http client提交

--- coroutine/4.x/http-client.php
+++ coroutine/4.x/http-client.php
@@ -2,6 +2,13 @@
 
 Co\run(function (){
     $cli = new Swoole\Coroutine\Http\Client('127.0.0.1', 8000);
+
+    $cli->set(
+        [
+            'timeout' => 3.0
+        ]
+    );
+
     $cli->get('/index.php');
     echo $cli->body;
     $cli->close();
