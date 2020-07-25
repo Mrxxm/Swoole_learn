a34084383f8166375470a889ba1538ffab2c7082
Mrxxm <362190221@qq.com>
2020-07-25 10:10:35 +0800

    swoole 协程 http server2 ws 提交

---
+++ coroutine/4.x/http-server2-ws.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * 此文件支持：http server & websocket server
+ */
+
+Co\run(function () {
+
+    $server = new Co\Http\Server("127.0.0.1", 9502, false);
+
+    $server->handle('/websocket', function ($request, $ws) {
+        $ws->upgrade(); // 向客户端发送websocket握手信息
+
+        while (true) {
+            $frame = $ws->recv();
+            if ($frame === false) {
+                echo "error : " . swoole_last_error() . "\n";
+                break;
+            } else if ($frame == '') {
+                break;
+            } else {
+                if ($frame->data == "close") {
+                    $ws->close();
+                    return;
+                }
+                $ws->push("Hello {$frame->data}!");
+                $ws->push("How are you, {$frame->data}?");
+            }
+        }
+    });
+
+
+
+    $server->start();
+});
